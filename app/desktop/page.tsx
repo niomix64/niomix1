854cb47b90242c0b00974d034f6cc8dcf7bb7aeb
niomix64 <sadkjdajs23y789@proton.me>
2024-09-20 20:53:13 +0300

    Update page.tsx

--- app/desktop/page.tsx
+++ app/desktop/page.tsx
@@ -4,10 +4,10 @@ import imageUrl from "@/public/qr.png"
 
 const DesktopPage = () => {
   return (
-    <main className="min-h-screen flex items-center justify-center gap-4 bg-[#f1582c] flex-col">
+    <main className="min-h-screen flex items-center justify-center gap-4 bg-[#b4b751] flex-col">
       <Image src={imageUrl} alt="qr" width={200} height={200} className="w-[200px] h-[200px]"/>
 
-        <p className="text-lg font-medium">Please, login on your mobile device!</p>
+        <p className="text-lg font-medium">Login on your mobile device!</p>
     </main>
   );
 };
